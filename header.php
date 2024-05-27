0f8eeaa728dd6bdff7f83d857a9f3fee1c61465e
tbg-webdiv <150207583+tbg-webdiv@users.noreply.github.com>
2024-05-27 11:42:22 +0900

    jqueryへの外部リンク記述を削除

--- header.php
+++ header.php
@@ -79,7 +79,6 @@ endif;
 <?php endif; ?>
 
 <?php wp_head(); ?>
-<script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
 </head>
 <body <?php
 if(is_page_template('pages/contact-tbgmail.php')){
