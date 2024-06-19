0f8d18d594ed74f255ebb59f1087e50f1114b901
atomura-m <m.atomura.tb@gmail.com>
2024-06-19 10:28:05 +0900

    一旦プッシュ

--- header.php
+++ header.php
@@ -78,6 +78,12 @@ endif;
 <meta name="description" content="<?php echo $description ?>">
 <?php endif; ?>
 
+<?php /* Googleフォント
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100..900&display=swap" rel="stylesheet">
+*/ ?>
+
 <?php wp_head(); ?>
 </head>
 <body <?php
